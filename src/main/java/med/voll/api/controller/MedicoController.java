5bd58009b2fb15be8c5ec41be143d45a3d97d4e0
Denison Kolling <denisonkolling@gmail.com>
2023-09-06 10:58:04 -0300

    feat: adiciona excluir medico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -37,4 +37,11 @@ public class MedicoController {
         medico.atualizarInformacoes(dados);
     }
 
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
+
 }
