2ebe06b8e8034abd3e915c2e2619cd9df32b2d54
Denison Kolling <denisonkolling@gmail.com>
2023-10-13 14:47:40 -0300

    fix: altera get email listagem pacientes

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -21,6 +21,7 @@ public class PacienteController {
     @PostMapping
     public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroPaciente dados, UriComponentsBuilder uriBuilder) {
         var paciente = new Paciente(dados);
+
         repository.save(paciente);
 
         var uri = uriBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
--- src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -5,7 +5,7 @@ import med.voll.api.endereco.Endereco;
 public record DadosListagemPaciente(Long id, String nome, String email, String telefone, Endereco endereco) {
 
     public DadosListagemPaciente(Paciente paciente){
-        this(paciente.getId(), paciente.getNome(), paciente.getCpf(), paciente.getTelefone(), paciente.getEndereco());
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getEndereco());
 
     }
 }
